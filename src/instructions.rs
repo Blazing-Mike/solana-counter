f83b2bb13a3a1cf1584a7f3b87ccb04b9cf882cb
Blazing-mike <adebambomichael5@gmail.com>
2024-03-08 23:23:18 +0100

    intial commit: counter and tests

---
+++ src/instructions.rs
@@ -0,0 +1,36 @@
+use borsh::BorshDeserialize;
+use borsh_derive::BorshSerialize;
+use solana_program::program_error::ProgramError;
+
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
+pub struct UpdateArgs {
+    pub value: u32,
+}
+
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
+pub struct Args {
+    pub value: u32,
+}
+
+pub enum CounterInstructions {
+    Increment(Args),
+    Decrement(Args),
+    Update(UpdateArgs),
+    Reset,
+}
+
+impl CounterInstructions {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+
+        Ok(match variant {
+            0 => Self::Increment(Args::try_from_slice(rest).unwrap()),
+            1 => Self::Decrement(Args::try_from_slice(rest).unwrap()),
+            2 => Self::Update(UpdateArgs::try_from_slice(rest).unwrap()),
+            3 => Self::Reset,
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
